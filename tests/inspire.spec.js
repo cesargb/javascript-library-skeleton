9d727ff332080b838a0105d8c13c2296991e57ba
Cesar <cesargb@gmail.com>
2024-05-07 07:11:35 +0200

    add test to browser

--- tests/inspire.spec.js
+++ tests/inspire.spec.js
@@ -1,6 +1,7 @@
-import { test, expectTypeOf } from 'vitest'
+import { test, expectTypeOf, expect } from 'vitest'
 import { inspire } from "../dist/myLibrary.js"
 
 test('inspire is a function', () => {
   expectTypeOf(inspire).toBeFunction()
+  expect(inspire()).toBeTypeOf('string')
 })
